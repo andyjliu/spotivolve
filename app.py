e5f1b2756910408af50fe3d64e76ac2d23c1f3e3
al1729 <andyliu1708@gmail.com>
2020-06-02 22:52:24 -0500

    misc backend improvements
    used artists endpoint for 2020, added display dictionary, fixed? the 50 limit thing

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
     return(redirect(auth_url))
 
 @app.route("/callback/q")
+
 def callback():
     #Redirect upon authorization
     songid_dict = {}
@@ -104,24 +105,29 @@ def callback():
         #update genre_counts dictionary
 
         artist_ids_str = ""
+        artist_ids_str_2 = ""
 
-        #TODO: split into even lists eventually instead of just taking first 50
+        #TODO: see if list splitting actually worked
         for a in range(50):
             try:
                 artist_ids_str += artist_ids[a]
                 artist_ids_str += ","
             except IndexError:
                 pass
+        if len(artist_ids) > 50:
+            for a in range(50):
+                try:
+                    artist_ids_str_2 += artist_ids[a+50]
+                    artist_ids_str_2 += ","
+                except IndexError:
+                    pass
+            artist_ids_str_2 = artist_ids_str_2[:-1]
         artist_ids_str = artist_ids_str[:-1]
-        '''for a in artist_ids:
-            artist_ids_str += a
-            artist_ids_str += ","
-        artist_ids_str = artist_ids_str[:-1]'''
+        
 
         genres_request = requests.get("http://api.spotify.com/v1/artists?ids=" + artist_ids_str, headers=authorization_header)
 
         list_of_artists = genres_request.json()["artists"]
-
         for i in range(0,len(list_of_artists)):
             genres = list_of_artists[i]["genres"]
             for g in genres:
@@ -129,12 +135,19 @@ def callback():
                     genre_counts[g] = genre_counts[g] + 1
                 except KeyError:
                     genre_counts[g] = 1
+        if len(artist_ids_str_2) > 1:
+            genres_request = requests.get("http://api.spotify.com/v1/artists?ids=" + artist_ids_str_2, headers=authorization_header)
+            for i in range(0,len(list_of_artists)):
+                genres = list_of_artists[i]["genres"]
+                for g in genres:
+                    try:
+                        genre_counts[g] = genre_counts[g] + 1
+                    except KeyError:
+                        genre_counts[g] = 1
 
         #update genres_dict with genre_counts dictionary
         genres_dict[year] = genre_counts
 
-    #get 2020 data from past six months' top tracks
-    #limited to 50 tracks (playlists were 100 tracks long)
     try:
         this_year_request = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0", headers=authorization_header)
         this_year = json.loads(this_year_request.text)["items"]
@@ -144,17 +157,11 @@ def callback():
     track_ids = []
     artist_ids = []
     genre_counts = {}
-    artist_counts = {}
 
     for i in range(0, len(this_year)):
         track_ids.append(this_year[i]["id"])
         artist_id = this_year[i]["album"]["artists"][0]["id"]
         artist_ids.append(artist_id)
-        artist_name = this_year[i]["album"]["artists"][0]["name"]
-        try:
-            artist_counts[artist_name] = artist_counts[artist_name] + 1
-        except KeyError:
-            artist_counts[artist_name] = 1
 
     artist_ids_str = ""
     for a in artist_ids:
@@ -175,8 +182,12 @@ def callback():
         
     genres_dict['2020'] = genre_counts
     songid_dict['2020'] = track_ids
+    artist_counts = {}
+    artists_request = requests.get("https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=5", headers=authorization_header)
+    for i in range(5):
+        artist = artists_request.json()['items'][i]['name']
+        artist_counts[artist] = 10-i
     artists_dict['2020'] = artist_counts
-
     audio_features = {}
 
     for year in songid_dict.keys():
@@ -184,19 +195,27 @@ def callback():
 
         danceability_sum = 0
         valence_sum = 0
-        tempo_sum = 0
-        loudness_sum = 0
+        energy_sum = 0
+        acousticness_sum = 0
         count = 0
-        #TODO: split into two even lists eventually instead of just taking first 50
         songstr = ""
+        songstr2 = ""
         for i in range(50):
             try:
                 songstr += songid_dict[year][i]
                 songstr += ","
             except IndexError:
                 pass
+        if len(songid_dict[year]) > 50:
+            for i in range(50):
+                try:
+                    songstr2 += songid_dict[year][i+50]
+                    songstr2 += ","
+                except IndexError:
+                    pass
 
         songstr = songstr[:-1]
+        songstr2 = songstr2[:-1]
         audiofeatures_request = requests.get("https://api.spotify.com/v1/audio-features/?ids="+songstr,headers=authorization_header)
         
         audiofeatures = audiofeatures_request.json()["audio_features"]
@@ -205,14 +224,36 @@ def callback():
             count += 1
             danceability_sum += af["danceability"]
             valence_sum += af["valence"]
-            tempo_sum += af["tempo"]
-            loudness_sum += af["loudness"]
+            energy_sum += af["energy"]
+            acousticness_sum += af["acousticness"]
 
-        audio_dict = {'danceability':danceability_sum/count,'valence':valence_sum/count,'tempo':tempo_sum/count,'loudness':loudness_sum/count}
+        audio_dict = {'danceability':danceability_sum/count,'valence':valence_sum/count,'energy':energy_sum/count,'acousticness':acousticness_sum/count}
         audio_features[year]=audio_dict
+        if len(songstr2) > 1:
+            audiofeatures_request = requests.get("https://api.spotify.com/v1/audio-features/?ids="+songstr2,headers=authorization_header)
+            audiofeatures = audiofeatures_request.json()["audio_features"]
+
+            for af in audiofeatures:
+                count += 1
+                danceability_sum += af["danceability"]
+                valence_sum += af["valence"]
+                energy_sum += af["energy"]
+                acousticness_sum += af["acousticness"]
 
     final_dict = {'audio_features':audio_features,'artists':artists_dict,'genres':genres_dict}
-    return(final_dict)
+
+    display_dict = {}
+    for i in final_dict['audio_features'].keys():
+        display_dict[i] = {}
+    for y in display_dict.keys():
+        display_dict[y]['danceability'] = final_dict['audio_features'][y]['danceability']
+        display_dict[y]['valence'] = final_dict['audio_features'][y]['valence']
+        display_dict[y]['acousticness'] = final_dict['audio_features'][y]['acousticness']
+        display_dict[y]['energy'] = final_dict['audio_features'][y]['energy']
+        display_dict[y]['artists'] = sorted(final_dict['artists'][y], key=final_dict['artists'][y].get, reverse=True)[:5]
+        display_dict[y]['genres'] = sorted(final_dict['genres'][y], key=final_dict['genres'][y].get, reverse=True)[:5]
+
+    return(display_dict)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
